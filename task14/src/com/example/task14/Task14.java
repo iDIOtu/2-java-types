a0a9511f7d3b3fbb57488d86c16158b804426eb8
nikit <nikitonnoev@gmail.com>
2024-09-19 20:09:55 +0500

    Голубев Никита. Задание 14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -5,9 +5,9 @@ public class Task14 {
 
     public static int reverse(int value) {
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        String number = Integer.toString(value);
+        String reverse_number = new StringBuilder(number).reverse().toString();
+        return Integer.parseInt(reverse_number);
     }
 
     public static void main(String[] args) {
